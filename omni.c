90bca05607d8c1eb71645e37c3caafc5bbc7e026
Hallison Boaventura <hallison.boaventura@itamaraty.gov.br>
2019-08-13 17:37:47 -0300

    tmp

--- omni.c
+++ omni.c
@@ -9,30 +9,32 @@
 #include "php_omni.h"
 #include "Zend/zend_smart_str.h"
 
-#define XFUNC_UNKNOWN        0x00
-#define XFUNC_NORMAL         0x01
-#define XFUNC_STATIC_MEMBER  0x02
-#define XFUNC_MEMBER         0x03
+#define FUNC_UNKNOWN        0x00
+#define FUNC_NORMAL         0x01
+#define FUNC_STATIC_MEMBER  0x02
+#define FUNC_MEMBER         0x03
 
-#define XFUNC_INCLUDES       0x10
-#define XFUNC_EVAL           0x10
-#define XFUNC_INCLUDE        0x11
-#define XFUNC_INCLUDE_ONCE   0x12
-#define XFUNC_REQUIRE        0x13
-#define XFUNC_REQUIRE_ONCE   0x14
-#define XFUNC_MAIN           0x15
+#define FUNC_INCLUDES       0x10
+#define FUNC_EVAL           0x10
+#define FUNC_INCLUDE        0x11
+#define FUNC_INCLUDE_ONCE   0x12
+#define FUNC_REQUIRE        0x13
+#define FUNC_REQUIRE_ONCE   0x14
+#define FUNC_MAIN           0x15
 
-#define XFUNC_ZEND_PASS      0x20
+#define FUNC_ZEND_PASS      0x20
 
 typedef struct _func_t {
     char *class;
     char *function;
     int   type;
-    int   internal;
 } func_t;
 
 typedef struct _function_stack_entry_t {
-    func_t function;
+    func_t  function;
+    char   *filename;
+    int     lineno;
+    char   *include_filename;
 } function_stack_entry_t;
 
 // ZEND_DECLARE_MODULE_GLOBALS(omni)
@@ -58,7 +60,7 @@ void my_error_cb(int type, const char *error_filename, const uint error_lineno, 
     original_zend_error_cb(type, error_filename, error_lineno, format, args_copy);
 }
 
-char *xdebug_sprintf(const char* fmt, ...)
+char *my_sprintf(const char* fmt, ...)
 {
     char   *new_str;
     int     size = 1;
@@ -108,7 +110,7 @@ char* wrap_closure_location_around_function_name(zend_op_array *opa, char *fname
     smart_str str = {0};
     smart_str_appendl(&str, fname, strlen(fname) - 1);
 
-    char *foo = xdebug_sprintf(
+    char *foo = my_sprintf(
         ":%s:%d-%d}",
         opa->filename->val,
         opa->line_start,
@@ -129,13 +131,13 @@ void build_fname(func_t *tmp, zend_execute_data *edata TSRMLS_DC)
 
 #if PHP_VERSION_ID >= 70100
     if (edata && edata->func && edata->func == (zend_function*) &zend_pass_function) {
-        tmp->type     = XFUNC_ZEND_PASS;
+        tmp->type     = FUNC_ZEND_PASS;
         tmp->function = estrdup("{zend_pass}");
     } else
 #endif
 
     if (edata && edata->func) {
-        tmp->type = XFUNC_NORMAL;
+        tmp->type = FUNC_NORMAL;
 
 #if PHP_VERSION_ID >= 70100
         if ((Z_TYPE(edata->This)) == IS_OBJECT) {
@@ -143,20 +145,20 @@ void build_fname(func_t *tmp, zend_execute_data *edata TSRMLS_DC)
         if (edata->This.value.obj) {
 #endif
 
-            tmp->type = XFUNC_MEMBER;
+            tmp->type = FUNC_MEMBER;
             if (edata->func->common.scope && strcmp(edata->func->common.scope->name->val, "class@anonymous") == 0) {
-                tmp->class = xdebug_sprintf(
+                tmp->class = estrdup(my_sprintf(
                     "{anonymous-class:%s:%d-%d}",
                     edata->func->common.scope->info.user.filename->val,
                     edata->func->common.scope->info.user.line_start,
                     edata->func->common.scope->info.user.line_end
-                );
+                ));
             } else {
                 tmp->class = estrdup(edata->This.value.obj->ce->name->val);
             }
         } else {
             if (edata->func->common.scope) {
-                tmp->type = XFUNC_STATIC_MEMBER;
+                tmp->type = FUNC_STATIC_MEMBER;
                 tmp->class = estrdup(edata->func->common.scope->name->val);
             }
         }
@@ -179,8 +181,8 @@ void build_fname(func_t *tmp, zend_execute_data *edata TSRMLS_DC)
             ((strncmp(edata->prev_execute_data->func->common.function_name->val, "assert", 6) == 0) ||
             (strncmp(edata->prev_execute_data->func->common.function_name->val, "create_function", 15) == 0))
         ) {
-            tmp->type = XFUNC_NORMAL;
-            tmp->function = estrdup("{internal eval}");
+            tmp->type = FUNC_NORMAL;
+            tmp->function = estrdup("{internal-eval}");
         } else if (
             edata &&
             edata->prev_execute_data &&
@@ -190,43 +192,60 @@ void build_fname(func_t *tmp, zend_execute_data *edata TSRMLS_DC)
         ) {
             switch (edata->prev_execute_data->opline->extended_value) {
                 case ZEND_EVAL:
-                    tmp->type = XFUNC_EVAL;
+                    tmp->type = FUNC_EVAL;
                     break;
                 case ZEND_INCLUDE:
-                    tmp->type = XFUNC_INCLUDE;
+                    tmp->type = FUNC_INCLUDE;
                     break;
                 case ZEND_REQUIRE:
-                    tmp->type = XFUNC_REQUIRE;
+                    tmp->type = FUNC_REQUIRE;
                     break;
                 case ZEND_INCLUDE_ONCE:
-                    tmp->type = XFUNC_INCLUDE_ONCE;
+                    tmp->type = FUNC_INCLUDE_ONCE;
                     break;
                 case ZEND_REQUIRE_ONCE:
-                    tmp->type = XFUNC_REQUIRE_ONCE;
+                    tmp->type = FUNC_REQUIRE_ONCE;
                     break;
                 default:
-                    tmp->type = XFUNC_UNKNOWN;
+                    tmp->type = FUNC_UNKNOWN;
                     break;
             }
         } else if (edata && edata->prev_execute_data) {
             build_fname(tmp, edata->prev_execute_data);
         } else {
-            tmp->type = XFUNC_UNKNOWN;
+            tmp->type = FUNC_UNKNOWN;
         }
     } // edata && edata->func
 }
 
-function_stack_entry_t *add_stack_frame(zend_execute_data *zdata)
+function_stack_entry_t *add_stack_frame(zend_execute_data *zdata, zend_op_array *op_array)
 {
     function_stack_entry_t *tmp = emalloc(sizeof(function_stack_entry_t));
+    tmp->include_filename = NULL;
+    tmp->filename = NULL;
+    tmp->lineno = 0;
+
+    zend_op  *cur_opcode;
+    zend_op **opline_ptr = (zend_op**) &zdata->opline;
+
+    tmp->filename = estrdup(op_array->filename->val);
+
     build_fname(&(tmp->function), zdata TSRMLS_CC);
 
     if (!tmp->function.type) {
         tmp->function.function = estrdup("{main}");
         tmp->function.class    = NULL;
-        tmp->function.type     = XFUNC_MAIN;
-    } else if (tmp->function.type & XFUNC_INCLUDES) {
+        tmp->function.type     = FUNC_MAIN;
+    } else if (tmp->function.type & FUNC_INCLUDES) {
+        tmp->lineno = 0;
+        if (opline_ptr) {
+            cur_opcode = *opline_ptr;
+            if (cur_opcode) {
+                tmp->lineno = cur_opcode->lineno;
+            }
+        }
 
+        tmp->include_filename = estrdup(zend_get_executed_filename(TSRMLS_C));
     }
 
     return tmp;
@@ -244,14 +263,33 @@ void profiler_function_end(function_stack_entry_t *fse TSRMLS_DC)
 
 void my_execute_ex(zend_execute_data *execute_data TSRMLS_DC)
 {
-    function_stack_entry_t *fse = add_stack_frame(execute_data);
-    zend_printf("->%s\n", fse->function.function);
+    zend_op_array          *op_array = &(execute_data->func->op_array);
+    function_stack_entry_t *fse = add_stack_frame(execute_data, op_array);
+
+    zend_printf("->");
+    if (fse->function.class != 0) {
+        const char *sep = fse->function.type == FUNC_STATIC_MEMBER ? "::" : "->";
+        zend_printf("%s%s", fse->function.class, sep);
+    }
+    zend_printf("%s at %s:%d (%s)\n", fse->function.function, fse->filename, fse->lineno, fse->include_filename);
 
     profiler_function_begin(fse TSRMLS_CC);
     original_zend_execute_ex(execute_data TSRMLS_CC);
     profiler_function_end(fse TSRMLS_CC);
 
     efree(fse->function.function);
+    if (fse->function.class != 0) {
+        efree(fse->function.class);
+    }
+
+    if (fse->filename != 0) {
+        efree(fse->filename);
+    }
+
+    if (fse->include_filename != 0) {
+        efree(fse->include_filename);
+    }
+
     efree(fse);
 }
 
@@ -270,8 +308,8 @@ PHP_MINIT_FUNCTION(omni)
 {
 	// REGISTER_INI_ENTRIES();
 
-    original_zend_error_cb = zend_error_cb;
-    zend_error_cb = my_error_cb;
+    // original_zend_error_cb = zend_error_cb;
+    // zend_error_cb = my_error_cb;
 
     original_zend_execute_ex = zend_execute_ex;
     zend_execute_ex = my_execute_ex;
@@ -283,7 +321,7 @@ PHP_MSHUTDOWN_FUNCTION(omni)
 {
 	// UNREGISTER_INI_ENTRIES();
 
-	zend_error_cb = original_zend_error_cb;
+	// zend_error_cb = original_zend_error_cb;
     zend_execute_ex = original_zend_execute_ex;
 
 	return SUCCESS;
---
+++ test2.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Foo\Bar;
+
+class Baz
+{
+    public function bazMethod()
+    {
+    }
+
+    public static function staticMethod()
+    {
+    }
+}
